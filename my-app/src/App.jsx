53dd54028a0321bf3d15a6d61930488be2ff33f0
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-08-23 14:28:16 +0530

    added react app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+// App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Hello React + Vite!</h1>
+      <p>This is a simple starter component.</p>
+    </div>
+  );
+}
+
+export default App;
